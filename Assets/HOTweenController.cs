18d5d62d977bd24ec1bc7197572f07f965c89b5d
Adam Telfer <adamtelfer@gmail.com>
2014-02-22 19:55:25 +0100

    telf - added finger gestures, first attempt at drawing path

---
+++ Assets/HOTweenController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+
+public class HOTweenController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		HOTween.Init(true, true, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PlayerControlComponent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+
+public class PlayerControlComponent : MonoBehaviour {
+
+	public GameObject playerObject;
+
+	public float playerSpeed = 3;
+	public EaseType easeType = EaseType.Linear;
+
+	void OnTap(TapGesture gesture) { 
+		Debug.Log( "Tap gesture detected at " + gesture.Position + 
+		          ". It was sent by " + gesture.Recognizer.name );
+
+		Vector3 worldPosition = Camera.main.ScreenToWorldPoint (new Vector3 (gesture.Position.x, gesture.Position.y, 0f));
+		worldPosition = new Vector3 (worldPosition.x, worldPosition.y, 0f);
+
+		//playerObject.transform.position = worldPosition;
+
+		HOTween.To(playerObject.transform, playerSpeed, new TweenParms()
+		           .Prop("position", worldPosition, false) // Position tween (set as relative)
+		           .Ease(easeType) // Ease
+		           );
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
